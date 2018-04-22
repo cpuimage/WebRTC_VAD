559c0f7c04ccd075694c8226746e1531973e3f4b
gaozhihan <gaozhihan@vip.qq.com>
2018-04-22 17:34:38 +0800

    remove wavWrite_int16

--- main.c
+++ main.c
@@ -19,24 +19,6 @@
 #define  MAX(A, B)        ((A) > (B) ? (A) : (B))
 #endif
 
-//写wav文件
-void wavWrite_int16(char *filename, int16_t *buffer, size_t sampleRate, size_t totalSampleCount) {
-    drwav_data_format format = {};
-    format.container = drwav_container_riff;     // <-- drwav_container_riff = normal WAV files, drwav_container_w64 = Sony Wave64.
-    format.format = DR_WAVE_FORMAT_PCM;          // <-- Any of the DR_WAVE_FORMAT_* codes.
-    format.channels = 1;
-    format.sampleRate = (drwav_uint32) sampleRate;
-    format.bitsPerSample = 16;
-    drwav *pWav = drwav_open_file_write(filename, &format);
-    if (pWav) {
-        drwav_uint64 samplesWritten = drwav_write(pWav, totalSampleCount, buffer);
-        drwav_uninit(pWav);
-        if (samplesWritten != totalSampleCount) {
-            fprintf(stderr, "ERROR\n");
-            exit(1);
-        }
-    }
-}
 
 //读取wav文件
 int16_t *wavRead_int16(char *filename, uint32_t *sampleRate, uint64_t *totalSampleCount) {
@@ -53,46 +35,6 @@ int16_t *wavRead_int16(char *filename, uint32_t *sampleRate, uint64_t *totalSamp
         *totalSampleCount = 0;
     }
     return buffer;
-}
-
-//分割路径函数
-void splitpath(const char *path, char *drv, char *dir, char *name, char *ext) {
-    const char *end;
-    const char *p;
-    const char *s;
-    if (path[0] && path[1] == ':') {
-        if (drv) {
-            *drv++ = *path++;
-            *drv++ = *path++;
-            *drv = '\0';
-        }
-    } else if (drv)
-        *drv = '\0';
-    for (end = path; *end && *end != ':';)
-        end++;
-    for (p = end; p > path && *--p != '\\' && *p != '/';)
-        if (*p == '.') {
-            end = p;
-            break;
-        }
-    if (ext)
-        for (s = end; (*ext = *s++);)
-            ext++;
-    for (p = end; p > path;)
-        if (*--p == '\\' || *p == '/') {
-            p++;
-            break;
-        }
-    if (name) {
-        for (s = p; s < end;)
-            *name++ = *s++;
-        *name = '\0';
-    }
-    if (dir) {
-        for (s = path; s < p;)
-            *dir++ = *s++;
-        *dir = '\0';
-    }
 }
 
 
@@ -139,7 +81,7 @@ int vadProcess(int16_t *buffer, uint32_t sampleRate, size_t samplesCount, int16_
     return 1;
 }
 
-void vad(char *in_file, char *out_file) {
+void vad(char *in_file) {
     //音频采样率
     uint32_t sampleRate = 0;
     //总音频采样数
@@ -151,7 +93,6 @@ void vad(char *in_file, char *out_file) {
         int16_t mode = 1;
         int per_ms = 30;
         vadProcess(inBuffer, sampleRate, inSampleCount, mode, per_ms);
-        wavWrite_int16(out_file, inBuffer, sampleRate, inSampleCount);
         free(inBuffer);
     }
 }
@@ -163,15 +104,7 @@ int main(int argc, char *argv[]) {
     if (argc < 2)
         return -1;
     char *in_file = argv[1];
-    char drive[3];
-    char dir[256];
-    char fname[256];
-    char ext[256];
-    char out_file[1024];
-    splitpath(in_file, drive, dir, fname, ext);
-    sprintf(out_file, "%s%s%s_out%s", drive, dir, fname, ext);
-    vad(in_file, out_file);
-
+    vad(in_file);
     printf("按任意键退出程序 \n");
     getchar();
     return 0;
